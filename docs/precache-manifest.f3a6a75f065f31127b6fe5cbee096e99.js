a340526159a69b01041793c6deec453896972f37
Sergi Lopez Moreno <lmsergi00@gmail.com>
2020-07-10 17:47:34 +0200

    docs agregado

---
+++ docs/precache-manifest.f3a6a75f065f31127b6fe5cbee096e99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb58fc932377a9eef398ea378e4f595",
+    "url": "/index.html"
+  },
+  {
+    "revision": "686eb72f9d18d43a6ea4",
+    "url": "/static/css/main.76f3c032.chunk.css"
+  },
+  {
+    "revision": "0e4f2a6108bd5d33be07",
+    "url": "/static/js/2.efe25c24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.efe25c24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686eb72f9d18d43a6ea4",
+    "url": "/static/js/main.d6139b78.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
